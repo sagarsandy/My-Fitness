61b5888d11ae1d63229b2ef630cdb80111ff7dde
Sagar <sagarsandy492@gmail.com>
2019-01-09 07:12:15 +0530

    Base commit

---
+++ My Fitness/My Fitness/Controller/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  My Fitness
+//
+//  Created by Sagar Sandy on 09/01/19.
+//  Copyright © 2019 Sagar Sandy. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ My Fitness/My Fitness/Controller/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  My Fitness
+//
+//  Created by Sagar Sandy on 09/01/19.
+//  Copyright © 2019 Sagar Sandy. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
